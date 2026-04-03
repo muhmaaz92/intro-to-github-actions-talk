e8b57cd64132d0c381aba7be1cb212b25fbacfa1
muhmaaz92 <muh.maaz92@gmail.com>
2026-04-03 23:15:12 +0500

    Update UnitTest1.cs fix

--- mywebapp/tests/UnitTest1.cs
+++ mywebapp/tests/UnitTest1.cs
@@ -8,12 +8,13 @@ namespace tests
         [Fact]
         public void Test1()
         {
-
+            // Empty tests pass by default in xUnit
         }
 
         [Fact]
         public void PassingTest()
         {
+            // This logic is correct: 2 + 2 = 4
             Assert.Equal(4, Add(2, 2));
         }
 
